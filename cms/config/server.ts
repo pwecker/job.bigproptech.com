2d1413457d87fb9788dcd3b550a8a8a1a09315ea
paulwecker <paulwecker504@gmail.com>
2025-09-22 18:13:59 -0500

    feat(cms): strapi google oauth

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -2,23 +2,13 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('BACK_URL'),
-  
+
   app: {
     keys: env.array('APP_KEYS'),
-    // koa
-    proxy: env('NODE_ENV') === 'production',
+    proxy: env('NODE_ENV') === 'production', // koa
   },
 
-  // railway
-  proxy: true,
-  middlewares: {
-    settings: {
-      cors: {
-        enabled: true,
-      },
-      trustProxy: true,
-    },
-  },
+  proxy: true, // strapi
   session: {
     cookie: {
       secure: env('NODE_ENV') === 'production',
