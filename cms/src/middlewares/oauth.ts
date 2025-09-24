3b0af242ca49c74da8f346ee17239c104b60702a
paulwecker <paulwecker504@gmail.com>
2025-09-24 07:42:19 -0500

    debug

--- cms/src/middlewares/oauth.ts
+++ cms/src/middlewares/oauth.ts
@@ -1,6 +1,5 @@
 export default () => {
   return async (ctx: any, next: any) => {
-    console.log(`oauth middleware: ${Date.now()}`)
     if (process.env.NODE_ENV === 'production') {
       const forwardedProto = ctx.headers['x-forwarded-proto'];
       if (forwardedProto === 'https') {
