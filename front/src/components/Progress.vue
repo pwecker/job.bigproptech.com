4c7e3dcedce6a0aa3e153667c71553df44f63961
paulwecker <paulwecker504@gmail.com>
2025-10-09 23:47:42 -0500

    feat(front): progress bar

---
+++ front/src/components/Progress.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+import { Progress } from '@/components/ui/progress'
+
+const progress = ref(13)
+
+watchEffect((cleanupFn) => {
+  const timer = setTimeout(() => progress.value = 66, 500)
+  cleanupFn(() => clearTimeout(timer))
+})
+</script>
+<template>
+  <Progress v-model="progress" class="w-3/5" />
+</template>
